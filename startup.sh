5ab8abc58ba1bc64bf8f2b63f183157c5d1b3e93
QAlerts <57975345+QAlerts@users.noreply.github.com>
2019-11-23 21:22:14 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,14 @@
 #~/bin/sh
 
 # This is just a sample of the file startup.sh which I placed in my ~/ (home) folder on the Linux Server installation.
+# sudo nano ~/startup.sh
+
+# After you create the file you will need to give it execute permissions.
+# chmod +x ~/startup.sh
+
+# I then edited ~/.profile and added the line ~/startup.sh
+# sudo nano ~/profile
+
 # Keep in mind Ubuntu Server LTS is terminal console only (no GUI desktop). It starts everything I need once I login.
 
 # Start the Loki network.
@@ -14,3 +22,5 @@ sleep 15
 
 # Start the sqraper.
 (cd ~/Sqraper && php ~/Sqraper/Sqraper.php)
+
+# Reboot, login and sqrape some Q!
